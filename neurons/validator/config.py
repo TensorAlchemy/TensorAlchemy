91e1308eef9155abcc46af5c3c9ac2da3bdb94fd
Emre Bayram <endemre@yahoo.com>
2024-07-29 11:15:24 +0300

    fix import error

--- neurons/validator/config.py
+++ neurons/validator/config.py
@@ -11,7 +11,6 @@ from loguru import logger
 from neurons.constants import (
     IS_TEST,
 )
-from neurons.validator.validator import StableValidator
 
 
 def get_default_device() -> torch.device:
@@ -255,5 +254,6 @@ def get_device(new_device: Optional[torch.device] = None) -> torch.device:
 def get_validator():
     global global_validator
     if global_validator is None:
+        from neurons.validator.validator import StableValidator
         global_validator = StableValidator()
     return global_validator
