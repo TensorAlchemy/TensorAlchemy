5bc5f6260a87527f7effe3d8ef9332a477da51f0
Will <Tavurth@gmail.com>
2025-01-22 14:53:50 +0700

    Cleanup unused tests

--- scoring/models/rewards/enhanced_clip/test_breakdown.py
+++ scoring/models/rewards/enhanced_clip/test_breakdown.py
@@ -5,9 +5,6 @@ import pytest
 from scoring.models.rewards.enhanced_clip.utils import (
     MissingApiKeyError,
     break_down_prompt,
-    get_prompt_breakdown_function,
-    get_query_messages,
-    process_api_response,
 )
 
 
@@ -16,44 +13,6 @@ def sample_prompt():
     return (
         "A serene lake surrounded by tall pine trees under a starry night sky"
     )
-
-
-def test_get_prompt_breakdown_function():
-    function = get_prompt_breakdown_function()
-    assert function["type"] == "function"
-    assert function["function"]["name"] == "break_down_prompt"
-    assert "parameters" in function["function"]
-
-
-def test_get_query_messages(sample_prompt):
-    messages = get_query_messages(sample_prompt)
-    assert len(messages) == 2
-    assert messages[0]["role"] == "system"
-    assert messages[1]["role"] == "user"
-    assert sample_prompt in messages[1]["content"]
-
-
-@pytest.mark.asyncio
-async def test_process_api_response():
-    mock_response = {
-        "choices": [
-            {
-                "message": {
-                    "tool_calls": [
-                        {
-                            "function": {
-                                "arguments": '{"elements": [{"description": "lake"}, {"description": "trees"}]}'
-                            }
-                        }
-                    ]
-                }
-            }
-        ]
-    }
-    result = await process_api_response(mock_response)
-    assert "elements" in result
-    assert len(result["elements"]) == 2
-    assert result["elements"][0]["description"] == "lake"
 
 
 @pytest.mark.asyncio
